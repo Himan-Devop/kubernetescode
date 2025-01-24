fbaad6fff85cc77c0dee98ea298f2b3294d0eee3
Himan-Devop <himanshusingh28200@gmail.com>
2025-01-24 14:28:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Is it working? '
+    return 'Yess sirrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr First project is Done '
